1b4e086d2ce95fa4218ea9009c908481f4c3103b
BelovUser <139882857+BelovUser@users.noreply.github.com>
2023-10-27 13:33:02 +0200

    Deleted RequestBody and add home page

--- SipmleWeb/src/main/java/com/example/sipmleweb/controller/WebController.java
+++ SipmleWeb/src/main/java/com/example/sipmleweb/controller/WebController.java
@@ -1,7 +1,19 @@
 package com.example.sipmleweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
+
+    @GetMapping("/webshop")
+    public String helloWorld(){
+        return "helloworld";
+    }
+
+    @GetMapping("/home")
+    public String websiteHome(){
+        return "header";
+    }
 }
