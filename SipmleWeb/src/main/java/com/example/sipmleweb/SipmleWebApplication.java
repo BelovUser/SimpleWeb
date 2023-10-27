73259bac41c5f6e813bb0da1ffad28964797b0b8
BelovUser <139882857+BelovUser@users.noreply.github.com>
2023-10-27 09:54:57 +0200

    Created project

---
+++ SipmleWeb/src/main/java/com/example/sipmleweb/SipmleWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.sipmleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipmleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SipmleWebApplication.class, args);
+    }
+
+}
---
+++ SipmleWeb/src/test/java/com/example/sipmleweb/SipmleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sipmleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipmleWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
